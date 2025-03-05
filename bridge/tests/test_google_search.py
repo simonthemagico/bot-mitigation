ad9e34ad0932e4a96513fc575a44a0b072420164
Sasha Bouloudnine <sasha.bouloudnine@lobstr.io>
2025-03-05 13:06:40 +0000

    [googlesearch_test] new test

--- bridge/tests/test_google_search.py
+++ bridge/tests/test_google_search.py
@@ -9,7 +9,9 @@ def test_google_bypass():
     print("=== Testing Google Search Bypass ===")
     
     bypass = GoogleSearchBypass(
-        proxy_pool="http://user-sp0e9f6467:08yf0pO2avT_mbiJNp@dc.smartproxy.com:20004",
+        # proxy_pool="http://user-sp0e9f6467:08yf0pO2avT_mbiJNp@dc.smartproxy.com:20004",
+        # proxy_pool="brd.superproxy.io:33335:brd-customer-hl_f9eb8d89-zone-zone19-ip-209.20.171.245:r32rfqmvys39",
+        proxy_pool="brd-customer-hl_f9eb8d89-zone-zone19-ip-185.255.164.144:r32rfqmvys39@brd.superproxy.io:33335",
         url="https://www.google.com/search?q=pizza",
         task_id="tk_test123",
         headless=False 
