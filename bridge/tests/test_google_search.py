543d11023c9219873bdc726d5faeb67393a736be
Sasha Bouloudnine <sasha.bouloudnine@lobstr.io>
2025-03-07 14:39:10 +0000

    [googlesearch] solve captcha properly

--- bridge/tests/test_google_search.py
+++ bridge/tests/test_google_search.py
@@ -11,9 +11,10 @@ def test_google_bypass():
     bypass = GoogleSearchBypass(
         # proxy_pool="http://user-sp0e9f6467:08yf0pO2avT_mbiJNp@dc.smartproxy.com:20004",
         # proxy_pool="brd.superproxy.io:33335:brd-customer-hl_f9eb8d89-zone-zone19-ip-209.20.171.245:r32rfqmvys39",
-        proxy_pool="brd-customer-hl_f9eb8d89-zone-zone19-ip-185.255.164.144:r32rfqmvys39@brd.superproxy.io:33335",
+        # proxy_pool="brd-customer-hl_f9eb8d89-zone-zone19-ip-185.255.164.144:r32rfqmvys39@brd.superproxy.io:33335",
+        proxy_pool="user-sp0e9f6467:08yf0pO2avT_mbiJNp@dc.smartproxy.com:10001",
         url="https://www.google.com/search?q=pizza",
-        task_id="tk_test123",
+        task_id="sasha",
         headless=False 
     )
     
